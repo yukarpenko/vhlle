31baac1c9796c3c57e47ce878236d30ffc16d580
jakubcimerman <jakub.cimerman@gmail.com>
2021-04-15 08:15:49 +0200

    Revert "Switch multihydro class to Milne coordinates"
    This reverts commit 8efe81ee55f58956c11eda58d56a0e3529212bf0.

--- src/multiHydro.cpp
+++ src/multiHydro.cpp
@@ -53,24 +53,21 @@ void MultiHydro::frictionSubstep()
     double ep, pp, nbp, nqp, nsp, vxp, vyp, vzp;
     double et, pt, nbt, nqt, nst, vxt, vyt, vzt;
     double ef, pf, nbf, nqf, nsf, vxf, vyf, vzf;
-    double taup = h_p->getTau();
-    double taut = h_t->getTau();
-    double tauf = h_f->getTau();
-    c_p->getPrimVar(eos, taup, ep, pp, nbp, nqp, nsp, vxp, vyp, vzp);
-    c_t->getPrimVar(eos, taut, et, pt, nbt, nqt, nst, vxt, vyt, vzt);
-    c_f->getPrimVar(eos, tauf, ef, pf, nbf, nqf, nsf, vxf, vyf, vzf);
+    c_p->getPrimVar(eos, h_p->getTau(), ep, pp, nbp, nqp, nsp, vxp, vyp, vzp);
+    c_t->getPrimVar(eos, h_t->getTau(), et, pt, nbt, nqt, nst, vxt, vyt, vzt);
+    c_f->getPrimVar(eos, h_f->getTau(), ef, pf, nbf, nqf, nsf, vxf, vyf, vzf);
     // 4-velocities, u_p and u_t
-    double gammap = 1.0/sqrt(1.0-vxp*vxp-vyp*vyp-vzp*vzp*taup*taup);
+    double gammap = 1.0/sqrt(1.0-vxp*vxp-vyp*vyp-vzp*vzp);
     double up [4] = {gammap,gammap*vxp,gammap*vyp,gammap*vzp};
-    double gammat = 1.0/sqrt(1.0-vxt*vxt-vyt*vyt-vzt*vzt*taut*taut);
+    double gammat = 1.0/sqrt(1.0-vxt*vxt-vyt*vyt-vzt*vzt);
     double ut [4] = {gammat,gammat*vxt,gammat*vyt,gammat*vzt};
-    double gammaf = 1.0/sqrt(1.0-vxf*vxf-vyf*vyf-vzf*vzf*tauf*tauf);
+    double gammaf = 1.0/sqrt(1.0-vxf*vxf-vyf*vyf-vzf*vzf);
     double uf [4] = {gammaf,gammaf*vxf,gammaf*vyf,gammaf*vzf};
     double flux_p [4] = {0.}, flux_t [4] = {0.};
      // 1. projectile-target friction
     if (ep>0. && et>0.) {
     // u_p^\mu u_t_\mu
-    double uput = gammap*gammat*(1.0 - vxp*vxt - vyp*vyt - vzp*vzt*taup*taut);
+    double uput = gammap*gammat*(1.0 - vxp*vxt - vyp*vyt - vzp*vzt);
     // s (Mandelstam) variable
     double s = 2.0*mN*mN*(1.0 + uput);
     double Ekin = s/(2.0*mN) - 2.0*mN;
@@ -89,7 +86,7 @@ void MultiHydro::frictionSubstep()
    // 2. projectile-fireball friction
    if(ep>0. && ef>0.) {
     double s = mpi*mpi + mN*mN + 2.*mpi*mN*gammaf*gammap*
-     (1.0 - vxf*vxp - vyf*vyp - vzf*vzp*tauf*taup);
+     (1.0 - vxf*vxp - vyf*vyp - vzf*vzp);
     double Vrel = 0.5/(mN*mpi)*sqrt(pow(s - mN*mN - mpi*mpi,2)
      - 4.*mN*mN*mpi*mpi);
     double D = Vrel*xsect->piN(s);
@@ -100,7 +97,7 @@ void MultiHydro::frictionSubstep()
    }
    if(et>0. && ef>0.) { // target-fireball friction
     double s = mpi*mpi + mN*mN + 2.*mpi*mN*gammaf*gammat*
-     (1.0 - vxf*vxt - vyf*vyt - vzf*vzt*tauf*taut);
+     (1.0 - vxf*vxt - vyf*vyt - vzf*vzt);
     double Vrel = 0.5/(mN*mpi)*sqrt(pow(s - mN*mN - mpi*mpi,2)
      - 4.*mN*mN*mpi*mpi);
     double D = Vrel*xsect->piN(s);
@@ -127,7 +124,7 @@ void MultiHydro::frictionSubstep()
 void MultiHydro::findFreezeout()
 {
  const double gmunu[4][4] = {
-     {1, 0, 0, 0}, {0, -1, 0, 0}, {0, 0, -1, 0}, {0, 0, 0, -h_p->getTau()}};
+     {1, 0, 0, 0}, {0, -1, 0, 0}, {0, 0, -1, 0}, {0, 0, 0, -1}};
  for (int iy = 0; iy < f_p->getNY(); iy++)
   for (int iz = 0; iz < f_p->getNZ(); iz++)
    for (int ix = 0; ix < f_p->getNX(); ix++) {
@@ -137,18 +134,15 @@ void MultiHydro::findFreezeout()
     double ep, pp, nbp, nqp, nsp, vxp, vyp, vzp;
     double et, pt, nbt, nqt, nst, vxt, vyt, vzt;
     double ef, pf, nbf, nqf, nsf, vxf, vyf, vzf;
-    double taup = h_p->getTau();
-    double taut = h_t->getTau();
-    double tauf = h_f->getTau();
-    c_p->getPrimVar(eos, taup, ep, pp, nbp, nqp, nsp, vxp, vyp, vzp);
-    c_t->getPrimVar(eos, taut, et, pt, nbt, nqt, nst, vxt, vyt, vzt);
-    c_f->getPrimVar(eos, tauf, ef, pf, nbf, nqf, nsf, vxf, vyf, vzf);
+    c_p->getPrimVar(eos, h_p->getTau(), ep, pp, nbp, nqp, nsp, vxp, vyp, vzp);
+    c_t->getPrimVar(eos, h_t->getTau(), et, pt, nbt, nqt, nst, vxt, vyt, vzt);
+    c_f->getPrimVar(eos, h_f->getTau(), ef, pf, nbf, nqf, nsf, vxf, vyf, vzf);
     // 4-velocities, u_p and u_t
-    double gammap = 1.0/sqrt(1.0-vxp*vxp-vyp*vyp-vzp*vzp*taup*taup);
+    double gammap = 1.0/sqrt(1.0-vxp*vxp-vyp*vyp-vzp*vzp);
     double up [4] = {gammap,gammap*vxp,gammap*vyp,gammap*vzp};
-    double gammat = 1.0/sqrt(1.0-vxt*vxt-vyt*vyt-vzt*vzt*taut*taut);
+    double gammat = 1.0/sqrt(1.0-vxt*vxt-vyt*vyt-vzt*vzt);
     double ut [4] = {gammat,gammat*vxt,gammat*vyt,gammat*vzt};
-    double gammaf = 1.0/sqrt(1.0-vxf*vxf-vyf*vyf-vzf*vzf*tauf*tauf);
+    double gammaf = 1.0/sqrt(1.0-vxf*vxf-vyf*vyf-vzf*vzf);
     double uf [4] = {gammaf,gammaf*vxf,gammaf*vyf,gammaf*vzf};
     double T [4][4] = {0.};
     for (int i=0; i<4; i++)
