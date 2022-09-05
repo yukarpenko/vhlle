148565e734e55fc0a1e00d892c49c0691d1ba575
Niklas Götz <goetz@fias.uni-frankfurt.de>
2022-09-05 11:07:11 +0200

    Fix bug related to negative baryon densities

--- src/eoCMF.cpp
+++ src/eoCMF.cpp
@@ -222,15 +222,16 @@ int EoSCMF::binary_search(double *a, int first, int last, double search_num) {
 int EoSCMF::get_ind(double eps, double n) {
 
  int nB_ind = (n + 0.5*dN) / dN;
+ if (nB_ind < 0) nB_ind = 0;
 
  double a[N_T];
 
  for (int i = 0; i < N_T; i++) {
   a[i] = etab[i][nB_ind];
  }
-
+ 
  int t_ind = binary_search(a, 0, N_T, eps);
-
+ if (t_ind >= N_T-1) t_ind = N_T-2;
 
  return t_ind;
 }
@@ -238,10 +239,9 @@ int EoSCMF::get_ind(double eps, double n) {
 double EoSCMF::get_temp(double eps, double n) {
  double el1, el2, eh1, eh2, elow, ehigh, etabval, ediff1, ediff2, temp;
  int n_ind = n / dN;
+ if (n_ind < 0) n_ind = 0;
 
  int t_ind = get_ind(eps, n);
-
-// printf("||| eps: %8.3e, n: %8.3e, ind: %d\n", eps, n, t_ind);
 
  el1 = etab[t_ind][n_ind];
  el2 = etab[t_ind][n_ind + 1];
