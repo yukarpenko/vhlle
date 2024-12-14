40215db108eed76a13dc491b23a3d4a74cf5662d
Robin Sattler <sattler@itp.uni-frankfurt.de>
2024-12-14 22:06:49 +0100

    Refactor VTK output config key and improve VTK code
    Removed unnecessary config key `VTK_output`. Now only the desired VTK
    output value has to be given in the config by using
    `VTK_output_values quantity` (e.g., `VTK_output_values eps`).
    Additionally, improved structure of VTK source code files vtk.cpp and
    vtk.h w.r.t. readability and added a check if quantities given in the
    config file are implemented VTK quantities. Prior to this commit, for
    each invalid quantity a VTK output file was produced which only
    contained the information of the function write_header. The check if
    a quantity is valid prevents this creation of basically empty files.

--- src/vtk.h
+++ src/vtk.h
@@ -1,49 +1,60 @@
-#include <iostream>
+#include <filesystem>
 #include <fstream>
 #include <iomanip>
-#include <filesystem>
+#include <iostream>
 #include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 class Cell;
-class Fluid;
 class EoS;
+class Fluid;
 class Hydro;
 
 class VtkOutput {
+  private:
+    std::string path_;
+    EoS* eos_;
+    double xmin_, ymin_, etamin_;
+    bool cartesian_;
+    int vtk_output_counter_ = 0;  // Number of vtk output in current event
+    /*
+     * The following vector contains all currently supported VTK quantities.
+     * If multiple quantities are desired, the delimiter in the config file has
+     * to be a comma without any whitespaces in between, for example:
+     * `VTK_output_valus eps,mub,nq,T`
+     */
+    const std::vector<std::string> valid_quantities_ = {
+      "eps", "mub", "muq", "mus", "nb", "nq", "ns", "p", "T"
+    };
 
- private:
+  public:
+    /**
+     * Create a new VTK output.
+     *
+     * \param path Path to the output file.
+     * \param eos The equation of state.
+     * \param xmin x coordinate of the first cell (center of fluid cell).
+     * \param ymin y coordinate of the first cell (center of fluid cell).
+     * \param etamin eta coordinate of the first cell (center of fluid cell).
+     * \param cartesian Bool that states if Cartesian output is enabled.
+     */
+    VtkOutput(std::string path, EoS* eos, double xmin, double ymin,
+              double etamin, bool cartesian=false):
+                path_(path),
+                eos_(eos),
+                xmin_(xmin),
+                ymin_(ymin),
+                etamin_(etamin),
+                cartesian_(cartesian)
+              {}
 
- std::string  path_;
- /// Number of vtk output in current event
- int vtk_output_counter_ = -1;
- EoS* eos_;
- bool cartesian_;
- double xmin_, ymin_,zmin_;
-
-
- public:
-  /**
-   * Create a new VTK output.
-   *
-   * \param path Path to the output file.
-   * \param name Name of the output.
-   * \param out_par Additional information on the configured output.
-   */
-  VtkOutput( std::string  path, EoS* eos, double xmin,double ymin,double zmin, bool cartesian=false):
-              path_(path),
-              eos_(eos),
-              cartesian_(cartesian),
-              xmin_(xmin),
-              ymin_(ymin),
-              zmin_(zmin)
-            {}
-
-  void write(const Hydro h, std::string &quantity);
-  void write_header(std::ofstream &file, const Hydro h,  const std::string &description);
-  void write_vtk_scalar(std::ofstream &file, const Hydro h, std::string &quantity);
-  std::string make_filename (const std::string &descr, int counter);
-
-  
+    void write(const Hydro h, std::string &quantity);
+    void write_header(std::ofstream &file, const Hydro h,
+                      const std::string &description);
+    void write_vtk_scalar(std::ofstream &file, const Hydro h,
+                          std::string &quantity);
+    bool is_quantity_implemented(std::string &quantity);
+    std::string make_filename (const std::string &descr, int counter);
 };
